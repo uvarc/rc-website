1fcd9a0976d3b67a33424a9d0b4a2541a13f81b9
nem2p <nmagee@virginia.edu>
2017-02-05 12:48:02 -0500

    Removing unused theme / renaming custom theme / events dynamic partial

--- publish.sh
+++ publish.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Ship the events CSV file
+aws s3 cp --acl public-read events.csv s3://somrc-data/events/
+
 echo "--- Clear the /public/ dir of all content"
 cd public/
 rm -R *
 cd ../
 echo "--- Content cleared"
 
-hugo --disableRSS --disable404 -v
+hugo --disableRSS --disable404 -v --ignoreCache
 echo "--- Hugo content generated"
 
+mkdir public/data
+cp events.csv public/data/
+echo "--- Events data copied"
 cp google091118a6c00d06c0.html public/
 echo "--- Google domain verification copied"
 cp robots.txt public/
