c3a79c40ff1a41ea5856df7c0d8a73c1df0de665
David Parsley <parsley@linuxjedi.org>
2019-04-26 10:48:08 -0400

    Add hugo to $PATH

--- .gitpod/get-hugo.sh
+++ .gitpod/get-hugo.sh
@@ -4,3 +4,5 @@ HUGO_VERSION="0.55.4"
 HUGO_BINARY="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
 
 curl -L "$HUGO_BINARY" | (cd /workspace; tar xzvf - hugo)
+
+ln -s /workspace/hugo /home/gitpod/go/bin/hugo
