6f05690c7f704785a4d8a32c4c3eab59cbc76a3d
Neal Magee <nmagee@virginia.edu>
2019-11-15 10:04:38 -0500

    Systems boilerplate. Cleanup form response messages

--- static/js/response-message.js
+++ static/js/response-message.js
@@ -9,12 +9,16 @@ if(status == "error" || status == undefined) {
   document.getElementById("form_post_response").innerHTML = message;
   var messagebox = document.getElementById("response_message");
   messagebox.classList.add("alert-danger");
-  document.getElementById("form_post_response").style.fontWeight = "500"
-} else {
+  document.getElementById("form_post_response").style.fontWeight = "500";
+} else if(status == "success") {
   success_message = "We're on it. RC staff make every effort to respond to requests within 1 business day.";
   full_message = "<b>" + message + "</b> " + success_message;
   document.getElementById("form_post_response").innerHTML = full_message;
   var messagebox = document.getElementById("response_message");
   messagebox.classList.add("alert-success");
-  document.getElementById("form_post_response").style.fontWeight = "500"
+  document.getElementById("form_post_response").style.fontWeight = "500";
+} else {
+  document.getElementById("form_post_response").innerHTML = message;
+  var messagebox = document.getElementById("response_message");
+  document.getElementById("form_post_response").style.fontWeight = "500";
 }
