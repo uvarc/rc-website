d4994010fe44ef1eff9bf32531a15f5c4142a322
Neal Magee <nem2p@virginia.edu>
2020-12-18 11:22:02 -0500

    Small fix to JS

--- static/js/user-session.js
+++ static/js/user-session.js
@@ -61,11 +61,11 @@ $('form').submit(function() {
   let email_dec = decode64(email);
   var set_email = document.getElementById("email").value = email_dec;
   
-  // get fname for friendliness
-  let fname_cookie = getCookie("__rc_fname");
-  let fname = decode64(fname_cookie);
-  // var set_greeting = document.getElementById("support-greeting").innerHTML = fname;
-  var set_greeting = document.getElementById("support-greeting");
-    if(set_greeting){
-      var set_g = document.getElementById("support-greeting").innerHTML = fname;
-    }
+  // // get fname for friendliness
+  // let fname_cookie = getCookie("__rc_fname");
+  // let fname = decode64(fname_cookie);
+  // // var set_greeting = document.getElementById("support-greeting").innerHTML = fname;
+  // var set_greeting = document.getElementById("support-greeting");
+  //   if(set_greeting){
+  //     var set_g = document.getElementById("support-greeting").innerHTML = fname;
+  //   }
