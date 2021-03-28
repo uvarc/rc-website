681b556a4511d2ee966ea567934d619e8a2d49c9
Neal Magee <nmagee@virginia.edu>
2021-03-28 19:46:27 -0400

    Testing clipboard fcn

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,12 +6,12 @@ function scrollFunction() {
     } else {
         document.getElementById("scrollBtn").style.display = "none";
     }
-}
+};
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
 // Sticky Things Below
 var stickySidebar = $('.sticky');
 if (stickySidebar.length > 0) { 
