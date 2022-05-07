4c318eafbfd6cac5e18348af17c7d4c1cfdb4287
Neal Magee <nem2p@virginia.edu>
2022-05-07 16:58:27 -0400

    Publish script reset

--- publish.sh
+++ publish.sh
@@ -7,6 +7,7 @@ echo ""
 echo "  ------------------------------------------------------- "
 echo "  --- Publishing with this method has been deprecated --- "
 echo "  --- Use this method only in break-glass situations  --- "
+echo "  --- Or for publishing preview deployments           --- "
 echo "  ------------------------------------------------------- "
 echo "  Simply commit your changes and 'git push origin BRANCH' "
 echo "  to deploy your code. The deployment pipeline will take  "
@@ -15,29 +16,14 @@ echo ""
 echo "**********************************************************"
 
 echo ""
-read -p "   Which branch do you want to publish: main, staging, or preview? [m/s/p]: " proceed
+read -p "   Which branch do you want to publish, main or staging? [m/s]: " proceed
 case $proceed in
     [m]* ) proceedx="1";;
     [s]* ) proceedx="2";;
-    [p]* ) proceedx="3";;
     * ) echo "Please answer m or s";;
 esac
 
-if [ $proceedx -eq 3 ]
-then
-  git checkout vpr
-  echo "You are publishing the VPR branch"
-  echo "--- Clear the /public/ dir of all content"
-  rm -Rf public/
-  mkdir public
-  echo "--- Content cleared"
-  hugo -v --ignoreCache    # try without cache
-  echo "--- Hugo content generated"
-  aws s3 sync --delete --cache-control max-age=86400 public/ s3://uvarc-website-preview/
-  echo "--- Public dir published to AWS"
-  echo "--- Preview URL: http://uvarc-website-preview.s3-website-us-east-1.amazonaws.com/"
-  exit 0
-elif [ $proceedx -eq 2 ]
+if [ $proceedx -eq 2 ]
 then
   git checkout staging
   echo "You are publishing the STAGING branch"
