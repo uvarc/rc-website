a7fac6cf9d0575f366962427932bfe8ff3a7effc
Neal Magee <nem2p@virginia.edu>
2022-08-17 20:39:55 -0400

    Better fallback for missing baked goods

--- static/js/user-session-v2.js
+++ static/js/user-session-v2.js
@@ -1,7 +1,24 @@
-  function getCookie(key) {
-    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-    return keyValue ? keyValue[2] : null;
-  };
+  // function getCookie(key) {
+  //   var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+  //   return keyValue ? keyValue[2] : null;
+  // };
+
+  function getCookie(c_name) {
+    var c_value = document.cookie,
+        c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) c_start = c_value.indexOf(c_name + "=");
+    if (c_start == -1) {
+        c_value = null;
+    } else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
   
   function setCookie(key, value, expiry) {
     var expires = new Date();
