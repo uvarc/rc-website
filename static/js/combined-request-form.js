b59f6283596a0a6a52bc20c8ce8cb9962c644bc1
JamesUVa <109172746+JamesUVa@users.noreply.github.com>
2025-03-07 11:02:45 -0500

    change validation

--- static/js/combined-request-form.js
+++ static/js/combined-request-form.js
@@ -1121,7 +1121,7 @@
     
             return errors; // Skip other validations for renewals
         }
-        if(isStorage){
+        if(isStorage && !isStorageChange){
             Object.values(payload[0].resources.storage).forEach((group, index) => {
                 // Check for required properties
                 if (!group.tier) {
