505aac951113d0185c704471882261aefa3c1d37
rvedhere <cyj7aj@virginia.edu>
2025-05-07 18:10:34 -0400

    FIXES

--- static/js/combined-request-form.js
+++ static/js/combined-request-form.js
@@ -817,10 +817,12 @@
         }
         if (isRetire ) {
             const requestType= formData.requestType==="storage" ? "storage" : "hpc_service-units";
-            const group=$('input[name="selected-st"]:checked').closest('tr').find('td').eq(3).text().trim();
-            const requestName = $('input[name="selected-st"]:checked').closest('tr').find('td').eq(3).text().trim()+"-"+$('input[name="selected-st"]:checked').closest('tr').find('td').eq(5).text().trim();
+            const group=$('input[name="selected-st"]:checked').closest('tr').find('td').eq(2).text().trim();
+            const requestName = $('input[name="selected-st"]:checked').closest('tr').find('td').eq(2).text().trim()+"-"+$('input[name="selected-st"]:checked').closest('tr').find('td').eq(4).text().trim();
             requestUrl += `?group_name=${group}&resource_request_type=${requestType}&resource_requst_name=${requestName}`;
         }
+        console.log("requestURL:"+requestUrl);
+        
         // Check for `trigger_notification` flag in payload
         if (isUpdateRequest && payload.length > 0 && payload[0].trigger_notification) {
             console.log("Triggering notification for renewal request.");
