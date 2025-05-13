f888746504987dffabde14400fa179e4b6ce162c
rvedhere <cyj7aj@virginia.edu>
2025-05-13 11:37:22 -0400

    button_fix

--- static/js/combined-request-form.js
+++ static/js/combined-request-form.js
@@ -536,7 +536,7 @@
         const isNew = $('#new-or-renewal-options input[name="new-or-renewal"]:checked').val() === 'new';
         const isRenew= $('#new-or-renewal-options input[name="new-or-renewal"]:checked').val() === 'renewal';
         if (isNew && !isRenew) {
-            $('#allocation-fields, #new-project-name-container, #project-description, #mygroups-group-container, #allocation-tier, #fdm_button').show();
+            $('#allocation-fields, #new-project-name-container, #project-description, #mygroups-group-container, #allocation-tier, #fdm_table').show();
             $('#existing-projects-allocation').hide();
         } else if(!isNew && isRenew) {
             $('#mygroups-group-container, #allocation-tier').hide();
