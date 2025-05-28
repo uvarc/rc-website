fef6a9bdd186c4fd91c8956fee02457e7d324250
rvedhere <cyj7aj@virginia.edu>
2025-05-28 19:45:21 -0400

    fix

--- static/js/combined-request-form.js
+++ static/js/combined-request-form.js
@@ -550,7 +550,7 @@
             $('#existing-projects-allocation').hide();
             billingData.fdm_billing_info = [];
         } else if(!isNew && isRenew) { 
-            $('#mygroups-group-container, #allocation-tier, #su-capacity').hide();
+            $('#mygroups-group-container, #allocation-tier, #su-capacity, #new-project-name-container, #project-description').hide();
             $('#existing-projects-allocation, #fdm_table, #fdm_button_div').show();
             populateExistingServiceUnitsTable(consoleData);
         } 
@@ -808,7 +808,6 @@
                 const tire = $parentRow[0].cells[4].textContent.trim();
                 const number = parseInt(match[1]);
                 console.log("Selected SUs:", number); 
-                $('#new-project-name-container, #project-description').show();
                 if(tire === "ssz_standard" || tire === "ssz_instructional" ) {
                     document.getElementById("su-capacity").style.display = "none"; 
                     $('#su-quantity').val(0); 
