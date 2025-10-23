f00aeafaf2abe16ab62f190e61a672f7c699ebda
rvedhere <cyj7aj@virginia.edu>
2025-10-23 12:15:54 -0400

    fix

--- static/js/resource-overview-page.js
+++ static/js/resource-overview-page.js
@@ -5,9 +5,13 @@
 $(document).ready(function () {
     // Remove unnecessary sections
     $(".blog-sidebar").remove();
-
     // Hide certain rows and remove required attributes
     ['#departmet_clasification_row', '#discipline_row'].forEach(r => $(r).hide());
     $('#discipline, #department, #classification').removeAttr('required');
+    const blogMain = document.querySelector(".blog-main");
+    if (blogMain) {
+       blogMain.classList.remove("col-sm-9");
+       blogMain.classList.add("col-sm-12");
+    }
 
 });
